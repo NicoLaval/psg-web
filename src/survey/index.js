5fcc749a5d438b8724097ed68622e03126d15440
NicoLaval <nicolas.laval@insee.fr>
2017-11-30 12:43:16 +0100

    Fix issue with adding adress field

--- src/survey/index.js
+++ src/survey/index.js
@@ -3,9 +3,14 @@ import Title from '../shared/title';
 import { GeneratedSurvey } from 'generated-survey';
 
 function Survey({ todo, survey, codeList, onClickBack, onClickSave }) {
+	const { number, street, postalCode, city } = todo.adress;
+	const adressLabel = `Adresse du ménage : ${number} ${street} - ${postalCode} - ${city}`;
 	return (
 		<div className="mui-container">
 			<Title title={`${survey.label}`} />
+			<div className="mui-row">
+				<h4 className="mui-col-md-12 centered">{adressLabel}</h4>
+			</div>
 			<div className="mui-row">
 				<GeneratedSurvey
 					todo={todo}
